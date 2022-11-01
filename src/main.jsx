a5e028d3f17d9dcfc29e1f801a845893c5fa5c02
antonio BC Energia <antonio.carlos@grupobcenergia.com.br>
2022-11-01 15:46:19 -0300

    Adicionado vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
+  createRoutesFromElements,
   createBrowserRouter,
   RouterProvider,
   Route,
@@ -16,60 +17,37 @@ import { action as destroyAction } from "./routes/destroy";
 import Index from "./routes/index";
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      { index: true, element: <Index /> },
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-        action: contactAction,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-        errorElement: <div>Oops! There was an error.</div>,
-      },
-    ],
-    // children: [
-    //   {
-    //     errorElement: <ErrorPage />,
-    //     children: [
-    //       { index: true, element: <Index /> },
-    //       {
-    //         path: "contacts/:contactId",
-    //         element: <Contact />,
-    //         loader: contactLoader,
-    //         action: contactAction,
-    //       },
-    //       {
-    //         path: "contacts/:contactId/edit",
-    //         element: <EditContact />,
-    //         loader: contactLoader,
-    //         action: editAction,
-    //       },
-    //       {
-    //         path: "contacts/:contactId/destroy",
-    //         action: destroyAction,
-    //         errorElement: <div>Oops! There was an error.</div>,
-    //       },
-    //     ]
-    //   }
-    // ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route
+          path="contacts/:contactId/destroy"
+          action={destroyAction}
+        />
+      </Route>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
